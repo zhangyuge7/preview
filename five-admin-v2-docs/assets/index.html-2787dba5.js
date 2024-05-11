3069f6ac97b70e30e18ca1528aa6c2377c0d6b6c
zhangyuge <398698424@qq.com>
2024-05-11 15:36:40 +0800

    update

--- five-admin-v2-docs/assets/index.html-2787dba5.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o,c,a as e}from"./app-d813bc76.js";const n="/preview/five-admin-v2-docs/footer.png",s={},_=e("p",null,[e("img",{src:n,alt:""})],-1),r=[_];function a(i,l){return o(),c("div",null,r)}const f=t(s,[["render",a],["__file","index.html.vue"]]);export{f as default};
